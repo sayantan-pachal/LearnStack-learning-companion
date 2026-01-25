7466a7b9c536fe753cc2f9ac82f5922324cfc39d
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-25 11:29:51 +0530

    update

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Mail, Lock, ArrowRight, Eye, EyeOff } from "lucide-react";
-import { hashPassword } from "../../utils/hash";
+import { hashPassword } from "./utils/hash";
 
 function Login() {
   const navigate = useNavigate();
--- src/components/Auth/ResetPassword.jsx
+++ src/components/Auth/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { hashPassword } from "../../utils/hash";
 import { Lock, Eye, EyeOff, ArrowRight } from "lucide-react";
+import { hashPassword } from "./utils/hash";
 
 
 function ResetPassword() {
