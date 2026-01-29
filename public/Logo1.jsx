0f13e8dae8fe538be8988279505f7a06d167d402
Sayantan Pachal <sayantanpachal006@gmail.com>
2026-01-30 00:39:37 +0530

    update header logo

--- public/Logo1.jsx
+++ public/Logo1.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Logo() {
     return (
         <div className="flex items-center space-x-2">
-            <img src="/Logo_images/logo1.png" alt="LearnStack Logo" className="w-10 h-10" />
+            <img src="/Logo_images/logo1.png" alt="LearnStack Logo" className="hidden lg:flex w-10 h-10" />
             <span className="text-xl font-bold text-gray-900 dark:text-white">
                 LearnStack
             </span>
